cd3af11d8cf8d72a30619aafa40c838020d4147e
PoiPo2 <kimmc0316@gmail.com>
2021-12-02 16:09:58 +0900

    2021-12-02 Reminder

--- job01_crawling.py
+++ job01_crawling.py
@@ -8,16 +8,6 @@
 # 크롤링한 데이터 파일은 아래 링크로 올려주세요.
 # https://drive.google.com/drive/folders/1vICarPIiRxa65pItqwBEwvMD_tlf0xQw?usp=sharing
 
-# Crawling 작업
-
-# Crawling은 각자 진행하고 빨리 완성되는 코드로 연도를 나눠서 진행하겠습니다.
-# 일단 2020년 개봉작만 크롤링 해주시고 저장 형식은 csv로 하겠습니다.
-# 나머지는 연도별로 나눠서 크롤링 해서 합칠게요.
-# 컬럼명은 ['title', 'reviews']로 통일해주세요.
-# 파일명은 reviews_0000.csv로 해주세요. 0000은 해당 연도입니다.
-# 크롤링한 데이터 파일은 아래 링크로 올려주세요.
-# https://drive.google.com/drive/folders/1vICarPIiRxa65pItqwBEwvMD_tlf0xQw?usp=sharing
-
 # 2021-12-02 Reminder
 # 한 사람당 1년치 분량만 Crawling 해주시면 되겠습니다.
 # 김민철 - 2016년
